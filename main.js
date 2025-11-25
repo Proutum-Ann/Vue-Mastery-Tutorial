e1c430a5c7de4f9d50b8217f6a073877e13ce21a
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-11-25 16:30:05 +0000

    List Rendering

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             description: 'Warm, soft socks for you and your feet.',
             url: 'https://www.vuemastery.com/',
             inventory: 100,
-            sale: true
+            sale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: ['Extra Small', 'Small', 'Medium', 'Large', 'Extra Large']
         }
     }
 })
